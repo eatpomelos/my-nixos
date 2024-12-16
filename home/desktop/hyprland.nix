62272a61d86c8748ea10627ce3ff0f5942150696
spikely <spikeluoyu@outlook.com>
2024-12-16 23:41:44 +0800

    update hyprland dotfile and wallpaper

--- home/desktop/hyprland.nix
+++ home/desktop/hyprland.nix
@@ -1,6 +1,9 @@
-{pkgs, ...}: {
+{config, pkgs, ...}: let
+  configPath = "${config.home.homeDirectory}/spk/my-nixos/home/desktop/hypr";
+in {
+  xdg.configFile."hypr".source = config.lib.file.mkOutOfStoreSymlink configPath;
   
-  	home.packages = with pkgs;[
+  home.packages = with pkgs;[
     waybar
     swaybg
     # hyprpaper
