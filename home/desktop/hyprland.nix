bcc02ba25a5d89c76a52ce48452993ff43ff613a
spikely <spikeluoyu@outlook.com>
2024-12-18 07:27:35 +0800

    add dotfile path

--- home/desktop/hyprland.nix
+++ home/desktop/hyprland.nix
@@ -1,11 +1,13 @@
 {config, pkgs, ...}: let
-  hyprland_configPath = "${config.home.homeDirectory}/spk/my-nixos/home/desktop/hypr";
-  waybar_configPath = "${config.home.homeDirectory}/spk/my-nixos/home/desktop/waybar";
-  mako_configPath = "${config.home.homeDirectory}/spk/my-nixos/home/desktop/mako";
+  hyprland_configPath = "${config.home.homeDirectory}/spk/my-nixos/home/desktop/dotfile/hypr";
+  waybar_configPath = "${config.home.homeDirectory}/spk/my-nixos/home/desktop/dotfile/waybar";
+  mako_configPath = "${config.home.homeDirectory}/spk/my-nixos/home/desktop/dotfile/mako";
+  tofi_configPath = "${config.home.homeDirectory}/spk/my-nixos/home/desktop/dotfile/tofi";
 in {
   xdg.configFile."hypr".source = config.lib.file.mkOutOfStoreSymlink hyprland_configPath;
   xdg.configFile."waybar".source = config.lib.file.mkOutOfStoreSymlink waybar_configPath;
   xdg.configFile."mako".source = config.lib.file.mkOutOfStoreSymlink mako_configPath;
+  xdg.configFile."tofi".source = config.lib.file.mkOutOfStoreSymlink tofi_configPath;
   
   home.packages = with pkgs;[
     waybar
